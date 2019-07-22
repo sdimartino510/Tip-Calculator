d94bebf83ae490eee4c29237052373b47498d879
Sergio Di Martino <sergio@webdevserg.io>
2019-07-21 22:57:08 -0700

    Initial commit

---
+++ calc.js
@@ -0,0 +1,38 @@
+var inquirer = require("inquirer");
+
+console.log("");
+console.log("Tip Calculator!");
+console.log("---------------");
+
+inquirer
+  .prompt([
+    {
+      type: "number",
+      name: "billAmount",
+      message: "How much is the bill?",
+      validate: function(value) {
+        var valid = !isNaN(parseFloat(value));
+        return valid || "Please enter a number";
+      }
+    },
+    {
+      type: "number",
+      name: "tipPercent",
+      message: "What percentage would you like to tip?",
+      validate: function(value) {
+        var valid = !isNaN(parseFloat(value));
+        return valid || "Please enter a number";
+      }
+    }
+  ])
+  .then(answers => {
+    tipCalc(answers.billAmount, answers.tipPercent);
+  });
+
+function tipCalc(billAmount, tipPercent) {
+  var tipDecimal = tipPercent / 100;
+  var tipAmount = tipDecimal * billAmount;
+  var billTotal = tipAmount + billAmount;
+  console.log("The amount of the tip is " + tipAmount);
+  console.log("Your total bill is " + billTotal);
+}
